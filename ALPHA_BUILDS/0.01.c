b384f058e4b4eb2dccc2e2faa13ab1e2ac413095
woodsjt19 <woodsj19@student.cbhs.school.nz>
2021-08-18 02:16:09 +1200

    made a new function for grabbing multichoice input

--- ALPHA_BUILDS/0.01.c
+++ ALPHA_BUILDS/0.01.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h> //nessesary for randomisation and literally nothing else
 #include <ctype.h> //i might remove this in future, i probably won't need it
-
+#include <string.h>
 //---VARIABLES---//
 //put global variables here//
 
@@ -10,8 +10,57 @@
 //put struct here//
 
 //---FUNCTIONS---//
-//put functions here
+//MULTICHOICE CHECKER (note: im writing this at 2am, if you dont understand this when you wake up tough luck lmao)
+char multi(char* prompt){ //note: the reason answer is an integer is because its multichoice, possible answers are 1, 2, 3, 4
+    for(;;){
+        printf("%s\n", prompt);
+        char userprompt[2];
+        fgets(userprompt, 2, stdin);
+        if((getchar()) != '\n'){
+            printf("invalid input\n");
+            while((getchar()) != '\n');
+            continue;
+        }
+        if(strcmp(userprompt, "A") != 0 && strcmp(userprompt, "a") != 0 && strcmp(userprompt, "b") != 0 && strcmp(userprompt, "B") != 0 && strcmp(userprompt, "c") != 0 && strcmp(userprompt, "C") != 0 && strcmp(userprompt, "D") != 0 && strcmp(userprompt, "d") != 0){
+            printf("invalid inpuy\n");
+            while((getchar()) != '\n'); // might not need this
+            continue;
+        }
+        if(strcmp(userprompt, "A") == 0 || strcmp(userprompt, "a") == 0){
+            printf("1\n");
+            return "A";
+        }
+        if(strcmp(userprompt, "B") == 0 || strcmp(userprompt, "b") == 0){
+            printf("2\n");
+            return "B";
+        }
+        if(strcmp(userprompt, "C") == 0 || strcmp(userprompt, "c") == 0){
+            printf("3\n");
+            return "C";
+        }
+        if(strcmp(userprompt, "D") == 0 || strcmp(userprompt, "d") == 0){
+            printf("4\n");
+            return "D";
+        }
+    }
+
+}
+
+int question(char* prompt, char* answer, int qtype, int points){
+    char userprompt;
+    if(qtype == 1){ //codeblock for doing multichoice questions
+        userprompt = multi(prompt);
+        if(strcmp(userprompt, answer) == answer){
+            printf("Correct!\n");
+            return points;
+        }
+        else{
+            print("incorrect\n");
+            return 0;
+        }
+    } // well thats the easy bit done prolly, good luck awake me lmaoooooo
+}
 
 int main(){
-    //do stuff here
+    question("Question\n");
 }
