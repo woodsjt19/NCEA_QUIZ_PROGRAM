41a10abea5be446efffaec5fbef1aebb44b4012f
woodsjt19 <woodsj19@student.cbhs.school.nz>
2021-08-17 13:13:32 +1200

    added alpha builds

---
+++ ALPHA_BUILDS/0.01.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h> //nessesary for randomisation and literally nothing else
+#include <ctype.h> //i might remove this in future, i probably won't need it
+
+//---VARIABLES---//
+//put global variables here//
+
+//---USERSTRUCT---//
+//put struct here//
+
+//---FUNCTIONS---//
+//put functions here
+
+int main(){
+    //do stuff here
+}
